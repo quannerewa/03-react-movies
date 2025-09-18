e22f84026b82beae0490f0f308b9f781f589af60
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2025-09-18 14:14:08 +0300

    upd token

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -12,7 +12,7 @@ interface MoviesHttpResponse {
 
 
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
-    const myKey = import.meta.env.VITE_API_KEY;
+    const myKey = import.meta.env.VITE_TMDB_TOKEN;
 
     const response = await axios.get<MoviesHttpResponse>(`https://api.themoviedb.org/3/search/movie?query=${query}`, {
         params: {},
